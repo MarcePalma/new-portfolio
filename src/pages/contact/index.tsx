646ba0fc694f701533775fde4d730079c85489df
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2024-05-22 00:39:47 -0300

    create Contact page

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <div>ContactPage
+        <p>
+            Hola aqui ira la pagina de contacto!
+        </p>
+    </div>
+  )
+}
